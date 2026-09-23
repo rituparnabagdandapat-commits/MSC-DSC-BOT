cba6fa10489fb4e4c798b775a3e015740f082010
rituparnabagdandapat-commits <rituparna.bagdandapat@gmail.com>
2026-09-23 16:11:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,18 @@
-from threading import Thread
+import os
+import discord
+from discord.ext import commands
 from flask import Flask
+from threading import Thread
 
+# 1. CREATE BOT FIRST!
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+# 2. FLASK STUFF
 app = Flask('')
-
 @app.route('/')
 def home():
-    return "MBS BOT IS ALIVE!"
+    return "MBS BOT IS ALIVE"
 
 def run():
   app.run(host='0.0.0.0', port=8080)
@@ -14,6 +21,11 @@ def keep_alive():
   t = Thread(target=run)
   t.start()
 
-# At the very bottom of your file
+# 3. YOUR BOT COMMANDS HERE
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+# 4. RUN AT VERY BOTTOM - LAST 2 LINES!
 keep_alive()
-bot.run(os.getenv("DISCORD_TOKEN"))  # or TOKEN
+bot.run(os.getenv("DISCORD_TOKEN"))
